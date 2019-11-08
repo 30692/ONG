8ff8d8f3b8f8cc51db50cd0f7c1e7c696d28bb65
unknown <30692@fiecalunos.lan>
2019-11-08 20:24:59 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Running');
+}) 
