c213e56e92dec01ebd6c7c26b5e447328ef79a35
unknown <30695@fiecalunos.lan>
2019-11-21 22:46:38 -0300

    anjinho

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ app.get('/auth', function(req, res) {
  
 // user gets sent here after being authorized
 app.get('/UserHasLoggedIn', function(req, res) {
-  res.json('OK Amiguinho feliz!');
+  res.sendfile(path.resolve("public/home2.html"));
 });
  
 
